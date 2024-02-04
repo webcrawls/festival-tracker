ceb8ff768c99077e883b7317aef1f826c348905e
webcrawls <git@kaden.sh>
2024-02-03 20:41:29 -0500

    ✨ Sorting methods

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,39 @@
     let songs: any[] = []
     let fuse: Fuse<string> = null
     let search: string = ""
+    let sortingMethod: string = "release"
+    $: sortingFn = sortingMethods[sortingMethod]
 
     $: searches = search === "" ? songs : fuse?.search(search).map(item => item.item)
+    $: searches = searches.sort(sortingFn)
+
+    const sortingMethods = {
+        'lead': (a, b) => {
+            const diffA = a.difficulties?.lead ?? 0
+            const diffB = b.difficulties?.lead ?? 0
+            return diffB - diffA
+        },
+        'vocals': (a, b) => {
+            const diffA = a.difficulties?.vocals ?? 0
+            const diffB = b.difficulties?.vocals ?? 0
+            return diffB - diffA
+        },
+        'drums': (a, b) => {
+            const diffA = a.difficulties?.drums ?? 0
+            const diffB = b.difficulties?.drums ?? 0
+            return diffB - diffA
+        },
+        'bass': (a, b) => {
+            const diffA = a.difficulties?.lead ?? 0
+            const diffB = b.difficulties?.lead ?? 0
+            return diffB - diffA
+        },
+        'bpm': (a, b) => {
+            const aBpm =  parseInt(a.bpm)
+            const bBpm = parseInt(b.bpm)
+            return bBpm- aBpm
+        }
+    }
 
     onMount(async () => {
         const res = await fetch("/songs.json")
@@ -36,6 +67,15 @@
     </header>
     <hr/>
     <input type="text" bind:value={search} placeholder="good 4 u"/>
+    <section>
+        <h3>Sort By</h3>
+        <select bind:value={sortingMethod}>
+            <option value="lead">Lead</option>
+            <option value="drums">Drums</option>
+            <option value="vocals">Vocals</option>
+            <option value="bass">Bass</option>
+        </select>
+    </section>
     <table class="songs">
         {#if !songs}
             <h1>Loading songs...</h1>
@@ -47,6 +87,10 @@
                 <th scope="col">Genre</th>
                 <th scope="col">Length</th>
                 <th scope="col">BPM</th>
+                <th on:click={() => sortingMethod = 'lead'} scope="col" class="instrument">Lead</th>
+                <th on:click={() => sortingMethod = 'vocals'} scope="col" class="instrument">Vocals</th>
+                <th on:click={() => sortingMethod = 'drums'} scope="col" class="instrument">Drums</th>
+                <th on:click={() => sortingMethod = 'bass'} scope="col" class="instrument">Bass</th>
             </tr>
         </thead>
         <tbody>
@@ -57,10 +101,15 @@
                     <td>{song.genre}</td>
                     <td>{song.length}</td>
                     <td>{song.bpm}</td>
+                    <td>{song?.difficulties?.lead ?? 'N/A'}</td>
+                    <td>{song?.difficulties?.vocals ?? 'N/A'}</td>
+                    <td>{song?.difficulties?.drums ?? 'N/A'}</td>
+                    <td>{song?.difficulties?.bass ?? 'N/A'}</td>
                 </tr>
             {/each}
         </tbody>
     </table>
+    <p style="font-style: italic; opacity: 0.7">hey. there may be some inconsistencies or incorrect information here. if you notice something wrong, leave an issue or suggest a change on GitHub!</p>
 </main>
 
 <style>
